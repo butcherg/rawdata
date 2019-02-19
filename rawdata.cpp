2d6bb61e921cf43e638118db4aa942217dca26c9
Glenn Butcher <glenn.butcher@gmail.com>
2019-02-18 20:44:42 -0700

    added EXIF tags

--- rawdata.cpp
+++ rawdata.cpp
@@ -6,6 +6,7 @@
 
 int main(int argc, char * argv[])
 {
+	char * img;
 	if (argc < 2) {
 		printf("rawdata: Opens a raw file from a digital camera,\n");
 		printf("extracts the raw image array, and saves it to a\n");
@@ -22,13 +23,18 @@ int main(int argc, char * argv[])
 		return 1; 
 	}
 
+	printf("Versions:\n\tlibraw=%s\n\ttiff=%s\n",LibRaw::version(), TIFFLIB_VERSION_STR);
+
 	//Use libraw to open the raw file and extract its parts:
 	LibRaw RawProcessor;
 	if (RawProcessor.open_file(argv[1]) != LIBRAW_SUCCESS) {
 		printf("Raw file open failed...\n"); 
 		return 1;
 	}
-	RawProcessor.unpack();
+	if (RawProcessor.unpack() != LIBRAW_SUCCESS) {
+		printf("Raw data unpack failed...\n"); 
+		return 1;
+	}
 
 	//Get the essential data from the opened raw file.
 	//raw_image is an array of 16-bit integers of size width*height:
@@ -54,7 +60,40 @@ int main(int argc, char * argv[])
 
 		//about the simplest TIFF format to write, one big strip:
 		TIFFSetField(tif, TIFFTAG_ROWSPERSTRIP, height);
-		TIFFWriteEncodedStrip(tif, 0, rawimage, width*height*2);
+		if (rawimage)
+			TIFFWriteEncodedStrip(tif, 0, rawimage, width*height*2);
+		else printf("rawimage has a problem...\n");
+
+		TIFFWriteDirectory( tif );
+
+		//add EXIF tags:
+		uint64 dir_offset = 0;
+		TIFFCreateEXIFDirectory(tif);
+
+		uint16 iso = (uint16) RawProcessor.imgdata.other.iso_speed;
+		if (!TIFFSetField(tif, EXIFTAG_ISOSPEEDRATINGS, 1, &iso)) 
+			printf("TIFFSetField failed\n");
+
+		double extime = (double) RawProcessor.imgdata.other.shutter;
+		if (!TIFFSetField(tif, EXIFTAG_EXPOSURETIME, extime)) 
+			printf("TIFFSetField failed\n");
+
+		double fnbr = (double) RawProcessor.imgdata.other.aperture;
+		if (!TIFFSetField(tif, EXIFTAG_FNUMBER, fnbr)) 
+			printf("TIFFSetField failed\n");
+
+		double flen = (double) RawProcessor.imgdata.other.focal_len;
+		if (!TIFFSetField(tif, EXIFTAG_FOCALLENGTH, flen)) 
+			printf("TIFFSetField failed\n");
+
+		if (!TIFFWriteCustomDirectory( tif, &dir_offset)) 
+			printf("TIFFWriteCustomDirectory failed\n");
+		if (!TIFFSetDirectory(tif, 0)) 
+			printf("TIFFSetDirectory failed\n");
+		if (!TIFFSetField(tif, TIFFTAG_EXIFIFD, dir_offset )) 
+			printf("TIFFSetField failed\n");
+
+
 
 		(void) TIFFClose(tif);
 
